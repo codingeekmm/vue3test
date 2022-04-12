e9e2af030d443a08f42e00d1d1fe1cad73eca473
MM <codingeekmm@gmail.com>
2022-04-12 17:07:30 +0630

    02_vue basic part1

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('log from vue js')
+const app= Vue.createApp({
+   
+    data(){
+        return {
+            title : 'The Final Empire',
+            author : 'James Willam',
+            age : 45
+        }
+    }
+})
+
+
+
+app.mount('#app')
